54d0a2e264102260e27cc2234d034b7db46948c5
Md Ishrak Mashroor <mdishrak.mashroor@ucalgary.ca>
2025-06-26 14:29:03 +0600

    update board.h

--- support/board.h
+++ support/board.h
@@ -1,6 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define BLUE "\033[34m"
+
+#define X 'X'
+#define O 'O'
+#define EMPTY ' '
+
 void print_board(char board[3][3]);
 
 #endif
