5b72f711c5884028bf20b95b23b3bb68b55c69dc
Md Ishrak Mashroor <mdishrak.mashroor@ucalgary.ca>
2025-07-26 22:38:01 +0600

    Create simple AI logic
    i still dont know how ill implement this but seems simple enough so far

--- support/GameAI.c
+++ support/GameAI.c
@@ -1,5 +1,20 @@
-#include "game.h"
-#include "board.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "board.h"
 
+// Function to make a random valid move for AI
+void ai_make_move(char board[3][3]) {
+    int row, col;
+    srand(time(NULL));
+
+    while (1) {
+        row = rand() % 3;
+        col = rand() % 3;
+        if (board[row][col] == EMPTY) {
+            board[row][col] = O;  // Computer is always O
+            printf("Computer played at: %d %d\n", row + 1, col + 1);
+            break;
+        }
+    }
+}
---
+++ support/GameAI.h
@@ -0,0 +1,6 @@
+#ifndef GAME_AI_H
+#define GAME_AI_H
+
+void ai_make_move(char board[3][3]);
+
+#endif
