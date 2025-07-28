2f452e42c84861b9154b423d6ab98c4f59613400
Md Ishrak Mashroor <mdishrak.mashroor@ucalgary.ca>
2025-07-28 22:21:34 +0600

    update gamebot

--- support/GameAI.c
+++ support/GameAI.c
@@ -7,10 +7,11 @@
 void ai_move(char board[3][3]) {
     int row, col;
     sleep(1); // Add 1 second time buffer
+    srand(time(NULL)); // Seed the random number generator
 
     while (1) {
-        row = rand() % 3;
-        col = rand() % 3;
+        row = rand() % 3; // Random row between 0 and 2
+        col = rand() % 3; // Random column between 0 and 2
         if (board[row][col] == EMPTY) {
             board[row][col] = O;  // Computer is always O
             printf("Computer played at: %d %d\n", row + 1, col + 1);
