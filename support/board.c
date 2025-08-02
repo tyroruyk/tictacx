49f2f8f75d711c462b12df77937d702c102417ae
Md Ishrak Mashroor <mdishrak.mashroor@ucalgary.ca>
2025-08-03 00:52:25 +0600

    documentation

--- support/board.c
+++ support/board.c
@@ -1,3 +1,16 @@
+/*
+ * Authors:
+  - 2524093042 – Avishek Dutta *(Lead)* mailto:avishek.dutta@example.com
+  - 2524709042 – Md Ishrak Mashroor mailto: ishrak.mashroor.252@northsouth.edu
+  - 2524767042 – Ashab Mahmud Raseen mailto:ashab.raseen@example.com
+  - 2524508042 – Sneha Nandy mailto:sneha.nandy@example.com
+
+ *
+ * Group: 02
+ *
+ * Version: 7.1
+ * Since: 1.0
+ */
 #include <stdio.h>
 #include "board.h"
 
