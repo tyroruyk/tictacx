27c24ceb6fec0fef08c552c5439e0be8712b7c87
sneha211398 <sneha211398@gmail.com>
2025-08-12 00:19:21 +0600

    Update game.h

--- support/game.h
+++ support/game.h
@@ -3,12 +3,12 @@
   - 2524093042 – Avishek Dutta *(Lead)* mailto:avishek.dutta@example.com
   - 2524709042 – Md Ishrak Mashroor mailto: ishrak.mashroor.252@northsouth.edu
   - 2524767042 – Ashab Mahmud Raseen mailto:ashab.raseen.252@northsouth.edu
-  - 2524508042 – Sneha Nandy mailto:sneha.nandy@example.com
+  - 2524508042 – Sneha Nandy mailto:sneha.nandy.252@northsouth.edu
 
  *
  * Group: 02
  *
- * Version: 1.1
+ * Version: 1.1.1
  * Since: 1.0
  */
 #ifndef GAME_H // include guard to prevent multiple inclusions
